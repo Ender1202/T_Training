10d1290983a87442398fd2f41203854c88c7704c
santuparsi <santuparsi@gmail.com>
2024-12-18 12:10:45 +0530

    commit5

---
+++ MVC/HandsOn/FileUploadDownload/FileUploadDownload/Controllers/FilesController.cs
@@ -0,0 +1,89 @@
+﻿using FileUploadDownload.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadDownload.Controllers
+{
+    public class FilesController : Controller
+    {
+        string conString = "Data Source=.;Initial Catalog =DemoTest; integrated security=true;";
+        static List<FileUpload> list = new List<FileUpload>();
+        // GET: Files
+        public ActionResult Index()
+        {
+            return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files)
+        {
+            FileUpload model = new FileUpload();
+          
+            
+
+            if (files != null)
+            {
+                var Extension = Path.GetExtension(files.FileName);
+                var fileName = "my-file-" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + Extension;
+                string path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+                model.FileUrl = Url.Content(Path.Combine("~/UploadedFiles/", fileName));
+                model.FileName = fileName;
+
+                
+                    files.SaveAs(path);
+                    TempData["AlertMessage"] = "Uploaded Successfully !!";
+                    list.Add(model);
+                    return RedirectToAction("Index");
+                
+            }
+            else
+            {
+                ModelState.AddModelError("", "Please Choose Correct File Type !!");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Files");
+        }
+
+       
+
+        private bool SaveFile(FileUpload model)
+        {
+            string strQry = "INSERT INTO tblFileDetails (FileName,FileUrl) VALUES('" +
+                model.FileName + "','" + model.FileUrl + "')";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand command = new SqlCommand(strQry, con);
+            int numResult = command.ExecuteNonQuery();
+            con.Close();
+            if (numResult > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public ActionResult DownloadFile(string filePath)
+        {
+            string fullName = Server.MapPath("~" + filePath);
+
+            byte[] fileBytes = GetFile(fullName);
+            return File(
+                fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
+        }
+    }
+}
